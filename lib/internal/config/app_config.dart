f101d94531bbc6ebb37fa26eb8c1ddaecfa8499f
matvuric <matviiv67@gmail.com>
2022-12-21 19:05:55 +0300

    laptop

--- lib/internal/config/app_config.dart
+++ lib/internal/config/app_config.dart
@@ -1,2 +1,2 @@
-String baseUrl = "http://192.168.56.1:5050/";
-String baseUrl2 = "http://192.168.56.1:5050";
+String baseUrl = "http://192.168.0.105:5050/";
+String baseUrl2 = "http://192.168.0.105:5050";
